db3081f39fed20b23407bed62a03ac510259dd17
paulleerentveld <paul@veldnet.com.au>
2022-03-14 15:41:57 +1000

    Add functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {name: 'Bob', streetAddress: '123 street'}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign(employee,{[key]:value});
+
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let result = {...employee};
+    delete result[key];
+    return result;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+
+}
+
